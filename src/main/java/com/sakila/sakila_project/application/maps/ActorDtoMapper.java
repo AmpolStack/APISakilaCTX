9db05138d4501cb552de95739cfa49a67baa77bf
Amapola <htrsanbl@gmail.com>
2025-04-05 19:22:24 -0500

    include the promised mappers

--- src/main/java/com/sakila/sakila_project/application/maps/ActorDtoMapper.java
+++ src/main/java/com/sakila/sakila_project/application/maps/ActorDtoMapper.java
@@ -1,4 +1,26 @@
 package com.sakila.sakila_project.application.maps;
 
-public class GeneralMapper {
+import com.sakila.sakila_project.application.dto.ActorWithFilmDto;
+import com.sakila.sakila_project.application.dto.FilmWithLanguageDto;
+import com.sakila.sakila_project.domain.model.sakila.Actor;
+import com.sakila.sakila_project.domain.model.sakila.Film;
+import com.sakila.sakila_project.domain.model.sakila.Language;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+
+@Mapper(componentModel = "spring")
+public interface ActorDtoMapper {
+
+    @Mapping(target = "language", qualifiedByName = "LanguageObjectToString")
+    public FilmWithLanguageDto toFilmWithLanguageDto(Film film);
+
+    public ActorWithFilmDto toActorWithFilmDto(Actor dto);
+
+    @Named("LanguageObjectToString")
+    default String toLanguageObjectToString(Language language) {
+        return language.getName();
+    }
+
 }
--- src/main/java/com/sakila/sakila_project/application/maps/MinimalDtoMapper.java
+++ src/main/java/com/sakila/sakila_project/application/maps/MinimalDtoMapper.java
@@ -1,4 +1,15 @@
 package com.sakila.sakila_project.application.maps;
 
-public class MinimalDtoMapper {
+import com.sakila.sakila_project.application.dto.MinimalActorDto;
+import com.sakila.sakila_project.domain.model.sakila.Actor;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MinimalDtoMapper {
+
+    public MinimalActorDto toMinimalActorDto(Actor dto);
+
+    public List<MinimalActorDto> toMinimalActorDtoList(List<Actor> dtoList);
 }
