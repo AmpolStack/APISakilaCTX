455acb59f1f5c6fc53fcb35f66cd2f2562df9d6d
Amapola <htrsanbl@gmail.com>
2025-04-07 17:05:43 -0500

    add staff controller and repository

--- src/main/java/com/sakila/sakila_project/infrastructure/adapters/input/rest/StaffController.java
+++ src/main/java/com/sakila/sakila_project/infrastructure/adapters/input/rest/StaffController.java
@@ -1,4 +1,42 @@
 package com.sakila.sakila_project.infrastructure.adapters.input.rest;
 
+import com.sakila.sakila_project.application.maps.MinimalDtoMapper;
+import com.sakila.sakila_project.domain.model.sakila.Staff;
+import com.sakila.sakila_project.infrastructure.adapters.output.repositories.sakila.StaffRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/staff")
 public class StaffController {
+
+    private StaffRepository repository;
+    private MinimalDtoMapper mapper;
+
+    @Autowired
+    public StaffController(StaffRepository repository, MinimalDtoMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+
+    @GetMapping("/getAllStaffs")
+    public ResponseEntity getAllStaffs(){
+        try{
+            List<Staff> staffs = repository.findAll();
+            var maps = this.mapper.toMinStaffDtoList(staffs);
+            return ResponseEntity.status(HttpStatus.OK).body(maps);
+        }
+        catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
 }
--- src/main/java/com/sakila/sakila_project/infrastructure/adapters/output/repositories/sakila/StaffRepository.java
+++ src/main/java/com/sakila/sakila_project/infrastructure/adapters/output/repositories/sakila/StaffRepository.java
@@ -1,4 +1,9 @@
 package com.sakila.sakila_project.infrastructure.adapters.output.repositories.sakila;
 
-public interface StaffRepository {
+import com.sakila.sakila_project.domain.model.sakila.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StaffRepository extends JpaRepository<Staff, Integer>{
 }
