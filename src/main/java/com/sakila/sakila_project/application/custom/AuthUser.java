2d2ad2d1cc7dc549a116d25f7ae558565671c2b6
Amapola <htrsanbl@gmail.com>
2025-04-09 23:47:11 -0500

    added implementation of UserDetails, step needed for authentication in spring security

---
+++ src/main/java/com/sakila/sakila_project/application/custom/AuthUser.java
@@ -0,0 +1,56 @@
+package com.sakila.sakila_project.application.custom;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class AuthUser implements UserDetails {
+
+    @Setter
+    private String username;
+    @Setter
+    private String password;
+    @Setter
+    @Getter
+    private String phone;
+    @Setter
+    @Getter
+    private String email;
+
+
+    //THE DATABASE NO CONTAINS ROLE ENTITIES YET, BECAUSE IS NEEDED TO WRITE THE ROLE HARDCODE
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"), new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+}
