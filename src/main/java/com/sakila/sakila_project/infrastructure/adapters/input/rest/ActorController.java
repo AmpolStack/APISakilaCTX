5bac7e0965fcd6ae4a0e3a772a5dea964623d019
Amapola <htrsanbl@gmail.com>
2025-05-02 21:42:25 -0500

    refactor(ActorController): using the redis database to store the prove code

--- src/main/java/com/sakila/sakila_project/infrastructure/adapters/input/rest/ActorController.java
+++ src/main/java/com/sakila/sakila_project/infrastructure/adapters/input/rest/ActorController.java
@@ -7,12 +7,15 @@ import com.sakila.sakila_project.domain.adapters.input.IEmailService;
 import com.sakila.sakila_project.infrastructure.adapters.output.repositories.sakila.ActorRepository;
 import com.sakila.sakila_project.infrastructure.adapters.output.repositories.sakila.FilmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/actors")
@@ -21,20 +24,28 @@ public class ActorController {
     @Value("${email.Username}")
     private String emailSenderAddress;
 
+    @Value("${operation.expiration.time}")
+    private int expirationRequestTime;
+    @Value("${operation.code.legth}")
+    private int codeLegth;
+
     private final ActorRepository actorRepository;
     private final FilmRepository filmRepository;
     private final MinimalDtoMapper minimalDtoMapper;
     private final ActorDtoMapper actorDtoMapper;
     private final IEmailService emailService;
+    private final RedisTemplate<String, Object> redisTemplate;
 
     @Autowired
     public ActorController(ActorRepository actorRepository, MinimalDtoMapper minimalDtoMapper, ActorDtoMapper actorDtoMapper,
-                           FilmRepository filmRepository, IEmailService emailService) {
+                           FilmRepository filmRepository, IEmailService emailService,
+                           @Qualifier("primaryRedisTemplate") RedisTemplate<String, Object> redisTemplate) {
         this.actorRepository = actorRepository;
         this.minimalDtoMapper = minimalDtoMapper;
         this.actorDtoMapper = actorDtoMapper;
         this.filmRepository = filmRepository;
         this.emailService = emailService;
+        this.redisTemplate = redisTemplate;
     }
 
     @GetMapping("/getAllActorsInfo")
@@ -146,7 +157,7 @@ public class ActorController {
 
     @GetMapping("/CheckEmail")
     public ResponseEntity<?> checkEmail(@RequestParam String email) {
-        var randomNumber = generateRandomNumber(8);
+        var randomNumber = generateRandomNumber();
         try{
             this.emailService.SendEmail(
                     "Security code in your created account",
@@ -154,6 +165,7 @@ public class ActorController {
                     emailSenderAddress,
                     email
                     );
+            this.redisTemplate.opsForValue().set(email, randomNumber, expirationRequestTime, TimeUnit.MINUTES);
             return ResponseEntity.ok(randomNumber);
         }
         catch(Exception e){
@@ -161,11 +173,34 @@ public class ActorController {
         }
     }
 
-    private static String generateRandomNumber(int length){
+    @GetMapping("/ProveCode")
+    public ResponseEntity<?> checkProveCode(@RequestParam int proveCode, @RequestParam String email) {
+        try{
+            var resp = this.redisTemplate.opsForValue().get(email);
+
+            if(resp == null){
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("The code no exist");
+            }
+
+            var cast = (String)resp;
+            if(!cast.equals(Integer.toString(proveCode))){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("The prove code is not equals");
+            }
+
+            var x = this.redisTemplate.opsForValue().getAndDelete(email);
+            return ResponseEntity.ok("Successfully prove code " + x);
+
+        }
+        catch(Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    private String generateRandomNumber(){
         var rnd = new Random();
-        var max = (int)Math.pow(10,length);
+        var max = (int)Math.pow(10,codeLegth);
         var number = rnd.nextInt(max);
-        return String.format("%0" + length + "d", number);
+        return String.format("%0" + codeLegth + "d", number);
     }
 
 }
