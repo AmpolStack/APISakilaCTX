b793138a5ddbfab976e40b603c09348fc5be2c3e
Amapola <htrsanbl@gmail.com>
2025-05-06 16:45:16 -0500

    fix(SakilaRepos): Implements the Query sentences in Jpa repositories

--- src/main/java/com/sakila/sakila_project/domain/ports/output/repositories/sakila/ActorRepository.java
+++ src/main/java/com/sakila/sakila_project/domain/ports/output/repositories/sakila/ActorRepository.java
@@ -6,9 +6,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ActorRepository extends JpaRepository<Actor, Integer> {
-    @Query("Select a FROM actor a JOIN fetch a.films")
-    public List<Actor> findActors();
+    @Query("Select a FROM actor a JOIN fetch a.films f JOIN FETCH f.language")
+    List<Actor> findAllWithFilmsAndLanguage();
+
+    @Query("Select a FROM actor a JOIN fetch a.films f JOIN FETCH f.language WHERE a.id = :id")
+    Optional<Actor> findByIdWithFilmsAndLanguage(int id);
+
 }
--- src/main/java/com/sakila/sakila_project/domain/ports/output/repositories/sakila/StaffRepository.java
+++ src/main/java/com/sakila/sakila_project/domain/ports/output/repositories/sakila/StaffRepository.java
@@ -2,11 +2,19 @@ package com.sakila.sakila_project.domain.ports.output.repositories.sakila;
 
 import com.sakila.sakila_project.domain.model.sakila.Staff;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface StaffRepository extends JpaRepository<Staff, Integer>{
-    Optional<Staff> findStaffByUsernameAndPassword(String username, String password);
+    @Query("SELECT s FROM Staff s JOIN FETCH s.address WHERE s.username = :username AND s.password = :password")
+    Optional<Staff> findByUsernameAndPasswordWithAddress(String username, String password);
+
+    @Query("SELECT s FROM Staff s JOIN FETCH s.store st JOIN FETCH s.address JOIN FETCH st.address WHERE s.id = :id")
+    Optional<Staff> findByIdWithStoreAndAddress(int id);
+
+    @Query("SELECT s FROM Staff s JOIN FETCH s.address WHERE s.id = :id")
+    Optional<Staff> findByIdWithAddress(int id);
 }
