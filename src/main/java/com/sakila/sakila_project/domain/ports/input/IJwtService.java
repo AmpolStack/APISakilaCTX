58158280b09cd8ab2bd36abe706d7df112e5ada5
Amapola <htrsanbl@gmail.com>
2025-05-13 03:20:17 -0500

    fix(JwtService): Implements the changes for Result Pattern

--- src/main/java/com/sakila/sakila_project/domain/ports/input/IJwtService.java
+++ src/main/java/com/sakila/sakila_project/domain/ports/input/IJwtService.java
@@ -1,15 +1,16 @@
 package com.sakila.sakila_project.domain.ports.input;
 
+import com.sakila.sakila_project.domain.results.Result;
 import io.jsonwebtoken.Claims;
 
 import java.util.*;
 
 public interface IJwtService {
-    String GenerateToken(Map<String, String> claims, int expiration, String secret);
-    String GenerateToken(Map<String, String> claims, String subject, int expiration, String secret);
+    Result<String> GenerateToken(Map<String, String> claims, int expiration, String secret);
+    Result<String> GenerateToken(Map<String, String> claims, String subject, int expiration, String secret);
     String GenerateRefreshToken();
-    boolean IsTokenExpired(String token, String secret);
-    List<Object> GetClaimsList(String token, String secret, Map<String, Class<?>> map);
-    <T> T GetClaim(String token, String secret, String claimName, Class<T> claimType);
-    Claims GetAllClaims(String token, String secret);
+    Result<Void> IsTokenExpired(String token, String secret);
+    Result<List<Object>> GetClaimsList(String token, String secret, Map<String, Class<?>> map);
+    <T> Result<T> GetClaim(String token, String secret, String claimName, Class<T> claimType);
+    Result<Claims> GetAllClaims(String token, String secret);
 }
--- src/main/java/com/sakila/sakila_project/infrastructure/adapters/output/JwtService.java
+++ src/main/java/com/sakila/sakila_project/infrastructure/adapters/output/JwtService.java
@@ -1,9 +1,14 @@
 package com.sakila.sakila_project.infrastructure.adapters.output;
 
 import com.sakila.sakila_project.domain.ports.input.IJwtService;
+import com.sakila.sakila_project.domain.results.Error;
+import com.sakila.sakila_project.domain.results.ErrorType;
+import com.sakila.sakila_project.domain.results.Result;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.WeakKeyException;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -12,23 +17,43 @@ import java.util.*;
 @Service
 public class JwtService implements IJwtService {
 
-    public String GenerateToken(Map<String, String> claims, int expiration, String secret) {
-        return Jwts.builder()
+    public Result<String> GenerateToken(Map<String, String> claims, int expiration, String secret) {
+
+        var errors = ProveMany(expiration, secret);
+        if(!errors.isEmpty()){
+            return Result.Failed(new Error(errors, ErrorType.VALIDATION_ERROR));
+        }
+
+        var builder = Jwts.builder()
                 .claims(claims)
                 .issuedAt(new Date())
-                .expiration(new Date(new Date().getTime() + expiration))
-                .signWith(GetSecretKey(secret))
-                .compact();
+                .expiration(new Date(new Date().getTime() + expiration));
+
+        try{
+            builder.signWith(GetSecretKey(secret));
+        }
+        catch (WeakKeyException ex){
+            return Result.Failed(new Error("This key is invalid", ErrorType.OPERATION_ERROR));
+        }
+
+        return Result.Success(builder.compact());
     }
 
-    public String GenerateToken(Map<String, String> claims, String subject, int expiration, String secret) {
-        return Jwts.builder()
+    public Result<String> GenerateToken(Map<String, String> claims, String subject, int expiration, String secret) {
+        var builder = Jwts.builder()
                 .subject(subject)
                 .claims(claims)
                 .issuedAt(new Date())
-                .expiration(new Date(new Date().getTime() + expiration))
-                .signWith(GetSecretKey(secret))
-                .compact();
+                .expiration(new Date(new Date().getTime() + expiration));
+
+        try{
+            builder.signWith(GetSecretKey(secret));
+        }
+        catch (WeakKeyException ex){
+            return Result.Failed(new Error("This key is invalid", ErrorType.PROCESSING_ERROR));
+        }
+
+        return Result.Success(builder.compact());
     }
 
     //128 Bits length
@@ -36,54 +61,107 @@ public class JwtService implements IJwtService {
         return UUID.randomUUID().toString();
     }
 
-    public boolean IsTokenExpired(String token, String secret) {
+    public Result<Void> IsTokenExpired(String token, String secret) {
         var claims = GetClaimsFromToken(token, secret);
-        return claims.getExpiration().after(new Date());
-    }
+        if(!claims.isSuccess()){
+            return Result.Failed(claims.getError());
+        }
 
-    public List<Object> GetClaimsList(String token, String secret, Map<String, Class<?>> map){
-        var claims = GetClaimsFromToken(token, secret);
-        var resultList = new ArrayList<>();
-
-        map.forEach((k, v) ->{
-            var valueClaim = claims.get(k);
-            if(v.isInstance(valueClaim)){
-                resultList.add(v);
-            }
-            else{
-                throw new ClassCastException("Invalid claim");
-            }
-        });
-        return resultList;
-    }
-
-
-    public <T> T GetClaim(String token, String secret, String claimName, Class<T> claimType){
-        var claims = GetClaimsFromToken(token, secret);
-        var claimValue = claims.get(claimName);
-        if(claimType.isInstance(claimType)){
-            return claimType.cast(claimValue);
+        if(!claims.getData().getExpiration().after(new Date())){
+            return Result.Failed(new Error("This token is expired", ErrorType.FAILURE_ERROR));
         }
         else{
-            throw new ClassCastException("Invalid claim");
+            return Result.Success();
         }
     }
 
-    public Claims GetAllClaims(String token, String secret){
+    public Result<List<Object>> GetClaimsList(String token, String secret, Map<String, Class<?>> map){
+        var claims = GetClaimsFromToken(token, secret);
+
+        if(!claims.isSuccess()){
+            return Result.Failed(claims.getError());
+        }
+
+        var valueClaims = claims.getData();
+        var resultList = new ArrayList<>();
+
+        for(var entry : map.entrySet()){
+            var valueClaim = valueClaims.get(entry.getKey());
+
+            if(entry.getValue().isInstance(valueClaim)){
+                resultList.add(entry.getValue());
+            }
+            else{
+                return Result.Failed(new Error("One claims are invalid to parse", ErrorType.OPERATION_ERROR));
+            }
+
+        }
+
+        return Result.Success(resultList);
+    }
+
+
+    public <T> Result<T> GetClaim(String token, String secret, String claimName, Class<T> claimType){
+
+        if(claimName.isBlank()){
+            return Result.Failed(new Error("claim name is blank", ErrorType.VALIDATION_ERROR));
+        }
+
+        var claims = GetClaimsFromToken(token, secret);
+
+        if(!claims.isSuccess()){
+            return Result.Failed(claims.getError());
+        }
+
+        var claimValue = claims.getData().get(claimName);
+        if(claimType.isInstance(claimType)){
+            var resp = claimType.cast(claimValue);
+            return Result.Success(resp);
+        }
+        else{
+            return Result.Failed(new Error("Invalid cast", ErrorType.OPERATION_ERROR));
+        }
+    }
+
+    public Result<Claims> GetAllClaims(String token, String secret){
         return GetClaimsFromToken(token, secret);
     }
 
-    private Claims GetClaimsFromToken(String token, String secret) {
-        return Jwts
+    private Result<Claims> GetClaimsFromToken(String token, String secret) {
+        if(token.isBlank()){
+            return Result.Failed(new Error("Token is blank", ErrorType.VALIDATION_ERROR));
+        }
+        if(secret.isBlank()){
+            return Result.Failed(new Error("Secret is blank", ErrorType.VALIDATION_ERROR));
+        }
+
+        var parser = Jwts
                 .parser()
                 .verifyWith(GetSecretKey(secret))
-                .build()
-                .parseSignedClaims(token)
-                .getPayload();
+                .build();
+        try{
+            var resp = parser.parseSignedClaims(token).getPayload();
+            return Result.Success(resp);
+        }
+        catch (JwtException | IllegalArgumentException ex){
+            return Result.Failed(new Error("Invalid token", ErrorType.OPERATION_ERROR));
+        }
     }
 
     private SecretKey GetSecretKey(String secret) {
         var keyBytes = secret.getBytes();
         return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    private List<String> ProveMany(int expiration, String secret){
+        var errors = new ArrayList<String>();
+
+        if(expiration < 0){
+            errors.add("The expiration must be greater than 0");
+        }
+        if(secret.isBlank()){
+            errors.add("The secret is mandatory");
+        }
+        return errors;
     }
 }
