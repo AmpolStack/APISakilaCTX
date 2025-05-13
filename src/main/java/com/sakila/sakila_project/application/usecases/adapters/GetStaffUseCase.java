e624ec4c582fbfe59464c32de16ebbabb1f517ab
Amapola <htrsanbl@gmail.com>
2025-05-13 03:25:29 -0500

    fix(GetStaffUseCase): Implements the Result Object

--- src/main/java/com/sakila/sakila_project/application/usecases/adapters/GetStaffUseCase.java
+++ src/main/java/com/sakila/sakila_project/application/usecases/adapters/GetStaffUseCase.java
@@ -10,10 +10,12 @@ import com.sakila.sakila_project.application.usecases.ports.IGetStaffUseCase;
 import com.sakila.sakila_project.domain.model.sakila.Staff;
 import com.sakila.sakila_project.domain.ports.output.ICacheService;
 import com.sakila.sakila_project.domain.ports.output.repositories.sakila.StaffRepository;
+import com.sakila.sakila_project.domain.results.Error;
+import com.sakila.sakila_project.domain.results.ErrorType;
+import com.sakila.sakila_project.domain.results.Result;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
@@ -37,54 +39,74 @@ public class GetStaffUseCase implements IGetStaffUseCase {
 
 
     @Override
-    public ExtendedStaffDto WithCompleteInfo(int id) {
+    public Result<ExtendedStaffDto> WithCompleteInfo(int id) {
         return getStaff(id, _staffDtoMapper::toDto, ExtendedStaffDto.class );
     }
 
     @Override
-    public BaseStaffDto WithBasicInfo(int id) {
+    public Result<BaseStaffDto> WithBasicInfo(int id) {
         return getStaff(id, _baseDtoMapper::toMinStaffDto, BaseStaffDto.class);
     }
 
 
-    private <T> T getStaff(int id, Function<Staff, T> mapFunction, Class<T> clazz){
+    private <T> Result<T> getStaff(int id, Function<Staff, T> mapFunction, Class<T> clazz){
 
-        var inCache = _cacheService.Get(Integer.toString(id));
+        var inCacheResult = _cacheService.Get(Integer.toString(id));
+        if(!inCacheResult.isSuccess()){
+            return Result.Failed(inCacheResult.getError());
+        }
+
+        var inCache = inCacheResult.getData();
         var serializer = new ObjectMapper();
 
         if(inCache==null){
             var map = _staffRepository
                     .findByIdWithStoreAndAddress(id)
                     .map(mapFunction)
-                    .orElseThrow(NoSuchElementException::new);
-            try {
-                var staffJson = serializer.writeValueAsString(map);
-                _cacheService.Set(Integer.toString(id), staffJson, 1, TimeUnit.MINUTES);
-            } catch (JsonProcessingException e) {
-                throw new IllegalStateException("Failed to serialize staff", e);
+                    .orElse(null);
+
+            if(map==null){
+                return Result.Failed(new Error("Staff not found with this id", ErrorType.NOT_FOUND_ERROR));
             }
 
-            return map;
+            String staffJson;
+
+            try {
+                staffJson = serializer.writeValueAsString(map);
+            } catch (JsonProcessingException e) {
+                return Result.Failed(new Error("Failed to serialize staff", ErrorType.OPERATION_ERROR));
+            }
+
+            var setCacheResp = _cacheService.Set(Integer.toString(id), staffJson, 1, TimeUnit.MINUTES);
+
+            if(!setCacheResp.isSuccess()){
+                return Result.Failed(setCacheResp.getError());
+            }
+
+            return Result.Success(map);
         }
         else{
+
             try {
-                return serializer.readValue(inCache, clazz);
+                var serialization = serializer.readValue(inCache, clazz);
+                return Result.Success(serialization);
             } catch (JsonProcessingException e) {
-                throw new IllegalStateException("Failed to deserialize staff", e);
+                return Result.Failed(new Error("Failed to deserialize staff", ErrorType.OPERATION_ERROR));
             }
+
         }
     }
 
     @Override
-    public List<BaseStaffDto> AllWithBasicInfo() {
+    public Result<List<BaseStaffDto>> AllWithBasicInfo() {
         var staffList = getStaffList();
-        return _baseDtoMapper.toMinStaffDtoList(staffList);
+        return Result.Success(_baseDtoMapper.toMinStaffDtoList(staffList));
     }
 
     @Override
-    public List<ExtendedStaffDto> AllWithCompleteInfo() {
+    public Result<List<ExtendedStaffDto>> AllWithCompleteInfo() {
         var staffList = getStaffList();
-        return _staffDtoMapper.toListDto(staffList);
+        return Result.Success(_staffDtoMapper.toListDto(staffList));
     }
 
     private List<Staff> getStaffList(){
--- src/main/java/com/sakila/sakila_project/application/usecases/ports/IGetStaffUseCase.java
+++ src/main/java/com/sakila/sakila_project/application/usecases/ports/IGetStaffUseCase.java
@@ -2,12 +2,13 @@ package com.sakila.sakila_project.application.usecases.ports;
 
 import com.sakila.sakila_project.application.dto.BaseStaffDto;
 import com.sakila.sakila_project.application.dto.ExtendedStaffDto;
+import com.sakila.sakila_project.domain.results.Result;
 
 import java.util.List;
 
 public interface IGetStaffUseCase {
-    ExtendedStaffDto WithCompleteInfo(int id);
-    List<ExtendedStaffDto> AllWithCompleteInfo();
-    BaseStaffDto WithBasicInfo(int id);
-    List<BaseStaffDto> AllWithBasicInfo();
+    Result<ExtendedStaffDto> WithCompleteInfo(int id);
+    Result<List<ExtendedStaffDto>> AllWithCompleteInfo();
+    Result<BaseStaffDto> WithBasicInfo(int id);
+    Result<List<BaseStaffDto>> AllWithBasicInfo();
 }
