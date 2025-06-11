fa2de7b63a52c0711747712cbbf75572e88d53ee
Amapola <htrsanbl@gmail.com>
2025-06-10 23:41:09 -0500

    feat: includes category repository

---
+++ src/main/java/com/sakila/sakila_project/domain/ports/output/repositories/sakila/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.sakila.sakila_project.domain.ports.output.repositories.sakila;
+
+import com.sakila.sakila_project.domain.model.sakila.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    @Query("SELECT c FROM Category c JOIN FETCH c.films f JOIN FETCH f.language")
+    List<Category> findAllCategoriesWithFilms();
+}
