17ff1874b256e57a9d027e4d17c509865706010f
Amapola <htrsanbl@gmail.com>
2025-06-10 23:46:44 -0500

    feat(FilmController): Add GET endpoint

--- src/main/java/com/sakila/sakila_project/infrastructure/adapters/input/rest/FilmController.java
+++ src/main/java/com/sakila/sakila_project/infrastructure/adapters/input/rest/FilmController.java
@@ -1,10 +1,25 @@
 package com.sakila.sakila_project.infrastructure.adapters.input.rest;
 
+import com.sakila.sakila_project.application.usecases.ports.film_operations.IGetFilmsUseCase;
+import com.sakila.sakila_project.infrastructure.utils.ResponseHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/films")
 public class FilmController {
+
+    private final IGetFilmsUseCase getFilmsUseCase;
+
+    public FilmController(IGetFilmsUseCase getFilmsUseCase) {
+        this.getFilmsUseCase = getFilmsUseCase;
+    }
+
+    @GetMapping("/getFilmsWithCategories")
+    public ResponseEntity<?> getFilmsWithCategories() {
+        return ResponseHandler.SendResponse(this.getFilmsUseCase::getFilms);
+    }
 
 }
