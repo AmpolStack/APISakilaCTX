3fa94955f4602bdd8a14867e9ffa0585dd9ec264
Amapola <htrsanbl@gmail.com>
2025-06-13 23:40:47 -0500

    feat: Update film operation interfaces

--- src/main/java/com/sakila/sakila_project/application/usecases/ports/film_operations/IFilmUseCase.java
+++ src/main/java/com/sakila/sakila_project/application/usecases/ports/film_operations/IFilmUseCase.java
@@ -1,17 +1,17 @@
 package com.sakila.sakila_project.application.usecases.ports.film_operations;
 
-import com.sakila.sakila_project.application.dto.Film.BaseFilmDto;
-import com.sakila.sakila_project.application.dto.Film.ExtendedFilmDto;
+import com.sakila.sakila_project.application.dto.film.FilmDto;
+import com.sakila.sakila_project.application.dto.film.FilmWithActorsAndCategoriesDto;
 import com.sakila.sakila_project.domain.results.Result;
 
 import java.util.List;
 
 public interface IFilmUseCase {
-    Result<ExtendedFilmDto> getFilmById(int id);
-    Result<ExtendedFilmDto> CreateFilm(ExtendedFilmDto extendedFilmDto);
-    Result<List<BaseFilmDto>> getAllFilmsWithBasicInfo();
-    Result<List<ExtendedFilmDto>> getAllFilmsWithExtendedInfo();
-    Result<ExtendedFilmDto> UpdateFilm(ExtendedFilmDto extendedFilmDto);
-    Result<ExtendedFilmDto> DeleteFilm(int filmId);
+    Result<FilmWithActorsAndCategoriesDto> getFilmById(int id);
+    Result<FilmWithActorsAndCategoriesDto> CreateFilm(FilmWithActorsAndCategoriesDto extendedFilmDto);
+    Result<List<FilmDto>> getAllFilmsWithBasicInfo();
+    Result<List<FilmWithActorsAndCategoriesDto>> getAllFilmsWithExtendedInfo();
+    Result<FilmWithActorsAndCategoriesDto> UpdateFilm(FilmWithActorsAndCategoriesDto extendedFilmDto);
+    Result<FilmWithActorsAndCategoriesDto> DeleteFilm(int filmId);
     Result<Void> AddActorInFilm(int filmId, String actorId);
 }
--- src/main/java/com/sakila/sakila_project/application/usecases/ports/film_operations/IGetFilmsUseCase.java
+++ src/main/java/com/sakila/sakila_project/application/usecases/ports/film_operations/IGetFilmsUseCase.java
@@ -1,14 +1,12 @@
 package com.sakila.sakila_project.application.usecases.ports.film_operations;
 
-import com.sakila.sakila_project.application.dto.Film.BaseFilmDto;
-import com.sakila.sakila_project.application.dto.Film.ExtendedFilmDto;
-import com.sakila.sakila_project.application.dto.category.BaseCategoryDto;
-import com.sakila.sakila_project.application.dto.category.ExtendedCategoryDto;
+import com.sakila.sakila_project.application.dto.film.FilmWithActorsAndCategoriesDto;
+import com.sakila.sakila_project.application.dto.category.CategoryWithFilmBasePropertiesDto;
 import com.sakila.sakila_project.domain.results.Result;
 
 import java.util.List;
 
 public interface IGetFilmsUseCase {
-    public Result<List<BaseCategoryDto>> getFilmsInfo();
-    public Result<ExtendedFilmDto> getFilm(String referenceId);
+    public Result<List<CategoryWithFilmBasePropertiesDto>> getFilmsInfo();
+    public Result<FilmWithActorsAndCategoriesDto> getFilm(String referenceId);
 }
