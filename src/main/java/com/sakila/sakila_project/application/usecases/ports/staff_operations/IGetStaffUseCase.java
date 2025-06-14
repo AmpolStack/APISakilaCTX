c23dcb1b5e35b350aa8aa22ccda270f8378de071
Amapola <htrsanbl@gmail.com>
2025-06-13 23:40:36 -0500

    feat: Update staff operation classes

--- src/main/java/com/sakila/sakila_project/application/usecases/ports/staff_operations/IGetStaffUseCase.java
+++ src/main/java/com/sakila/sakila_project/application/usecases/ports/staff_operations/IGetStaffUseCase.java
@@ -1,14 +1,14 @@
 package com.sakila.sakila_project.application.usecases.ports.staff_operations;
 
-import com.sakila.sakila_project.application.dto.BaseStaffDto;
-import com.sakila.sakila_project.application.dto.ExtendedStaffDto;
+import com.sakila.sakila_project.application.dto.staff.StaffDto;
+import com.sakila.sakila_project.application.dto.staff.StaffWithStoreAndAddressDto;
 import com.sakila.sakila_project.domain.results.Result;
 
 import java.util.List;
 
 public interface IGetStaffUseCase {
-    Result<ExtendedStaffDto> WithCompleteInfo(int id);
-    Result<List<ExtendedStaffDto>> AllWithCompleteInfo();
-    Result<BaseStaffDto> WithBasicInfo(int id);
-    Result<List<BaseStaffDto>> AllWithBasicInfo();
+    Result<StaffWithStoreAndAddressDto> WithCompleteInfo(int id);
+    Result<List<StaffWithStoreAndAddressDto>> AllWithCompleteInfo();
+    Result<StaffDto> WithBasicInfo(int id);
+    Result<List<StaffDto>> AllWithBasicInfo();
 }
--- src/main/java/com/sakila/sakila_project/application/usecases/ports/staff_operations/IMutableStaffUseCase.java
+++ src/main/java/com/sakila/sakila_project/application/usecases/ports/staff_operations/IMutableStaffUseCase.java
@@ -1,12 +1,12 @@
 package com.sakila.sakila_project.application.usecases.ports.staff_operations;
 
-import com.sakila.sakila_project.application.dto.BaseAddressDto;
-import com.sakila.sakila_project.application.dto.BaseStaffDto;
-import com.sakila.sakila_project.application.dto.ExtendedStaffDto;
+import com.sakila.sakila_project.application.dto.others.AddressDto;
+import com.sakila.sakila_project.application.dto.staff.StaffDto;
+import com.sakila.sakila_project.application.dto.staff.StaffWithStoreAndAddressDto;
 import com.sakila.sakila_project.domain.results.Result;
 
 public interface IMutableStaffUseCase {
-    Result<BaseStaffDto> updateAllStaffProperties(BaseStaffDto baseStaffDto, int staffId);
-    Result<ExtendedStaffDto> updateAddresses(BaseAddressDto address, int staffId6);
+    Result<StaffDto> updateAllStaffProperties(StaffDto baseStaffDto, int staffId);
+    Result<StaffWithStoreAndAddressDto> updateAddresses(AddressDto address, int staffId6);
     Result<Void> updateAssignedStore(int storeId, int staffId);
 }
